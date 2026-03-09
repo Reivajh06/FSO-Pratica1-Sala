3c90253f1de9b921e8a794a70095435764a7f3c6
ReivajH06 <reivajh06@gmail.com>
2026-03-09 12:36:00 +0000

    Added documentation

--- sala.c
+++ sala.c
@@ -11,6 +11,12 @@ unsigned int* seats = NULL;
 unsigned int empty_seats;
 unsigned int occupied_seats;
 
+/**
+ * Busca por el vector de asientos cualquier posición libre y asigna en ese asiento el id de la persona a sentar
+ *
+ * @param id_persona id de la persona a sentar
+ * @return -1 si el vector de asientos no está inicializado o la sala está llena o el id del primer asiento que se encontró libre
+ */
 int reserva_asiento(int id_persona) {
     if(seats == NULL || id_persona < 0 || occupied_seats == CAPACIDAD) return - 1;
 
@@ -27,6 +33,14 @@ int reserva_asiento(int id_persona) {
     return -1;
 }
 
+/**
+ * Retorna el id de la persona sentada en el asiento especificado,
+ * indicando que dicho asiento ahora esta vacío (0)
+ *
+ * @param id_asiento id del asiento a liberar
+ * @return -1 si la sala no está inicializada o está vacío el asiento o el id de la persona sentada
+ * en el asiento especificado
+ **/
 int libera_asiento(int id_asiento) {
     if(seats == NULL || id_asiento < 0) return -1;
 
@@ -41,6 +55,12 @@ int libera_asiento(int id_asiento) {
     return seat;
 }
 
+/**
+ * Determina el estado actual del asiento especificado
+ *
+ * @param id_asiento id del asiento pasado por parámetro
+ * @return -1 si el vector de asientos no está inicializado o el id es inválido o el estado del asiento especificado
+ */
 int estado_asiento(int id_asiento) {
     if(seats == NULL) return -1;
 
@@ -51,24 +71,46 @@ int estado_asiento(int id_asiento) {
     return seat;
 }
 
+/**
+ * Determina el número de asientos libres en la sala
+ *
+ * @return -1 si el vector de asientos no está inicializado, sino el número de asientos vacíos
+ */
 int asientos_libres() {
     if(seats == NULL) return -1;
 
     return empty_seats;
 }
 
+/**
+ * Determina el número de asientos ocupados en la sala
+ *
+ * @return -1 si el vector de asientos no está inicializado, sino el número de asientos ocupados
+ */
 int asientos_ocupados() {
     if(seats == NULL) return -1;
 
     return occupied_seats;
 }
 
+/**
+ * Determina la capacidad de la sala, especificada cuando se inicializó el vector de asientos
+ *
+ * @return -1 si el vector de asientos no está inicializado, sino la capacidad del vector
+ */
 int capacidad_sala() {
     if(seats == NULL) return -1;
 
     return CAPACIDAD;
 }
 
+/**
+ * Reserva memoria para inicializar el vector de asientos, dada la capacidad del mismo
+ *
+ * @param capacidad longitud que tendrá el vector de asientos
+ * @return -1 si la capacidad es inválida o ya está inicializado el vector de asientos (también por posibles fallos al alojar memoria),
+ * sino devuelve la capacidad especificada del vector
+ */
 int crea_sala(int capacidad) {
     if (capacidad <= 0) return -1;
 
@@ -91,6 +133,11 @@ int crea_sala(int capacidad) {
     return capacidad;
 }
 
+/**
+ * Libera la memoria utilizada por el vector de asientos
+ *
+ * @return -1 si el vector no está inicializado, o 0 si se realizó con éxito
+ */
 int elimina_sala() {
     if(seats == NULL) {
         return -1;
--- test_sala.c
+++ test_sala.c
@@ -42,11 +42,38 @@ void test_ReservaBasica()
     FIN_TEST("Reserva básica");
 }
 
+void test_sala() {
+    int mi_asiento;
+    #define CAPACIDAD 500
+    #define ID_1 1500;
+    INICIO_TEST("Sala");
+    DebeSerCierto(capacidad_sala() == -1);
+    DebeSerCierto(estado_asiento(50) == -1);
+    DebeSerCierto(libera_asiento(20) == -1);
+    DebeSerCierto(reserva_asiento(20) == -1);
+    DebeSerCierto(asientos_ocupados() == -1);
+    DebeSerCierto(asientos_libres() == -1);
+    DebeSerCierto(capacidad_sala() == -1);
+    DebeSerCierto(crea_sala(CAPACIDAD) == CAPACIDAD);
+    DebeSerCierto(crea_sala(CAPACIDAD) == -1);
+    DebeSerCierto(capacidad_sala() == CAPACIDAD);
+    DebeSerCierto(estado_asiento(50) == 0);
+    DebeSerCierto(reserva_asiento(20) == 1);
+    DebeSerCierto(estado_asiento(1) == 20);
+    DebeSerCierto(libera_asiento(1) == 20);
+    for (int i = 1; i <= CAPACIDAD; i++) {
+        DebeSerCierto(reserva_asiento(i) == i);
+    }
+    elimina_sala();
+    FIN_TEST("Reserva ");
+}
+
 
 void ejecuta_tests ()
 {
     test_ReservaBasica();
     // Añadir nuevos tests
+    test_sala();
 }
 
 int main()
